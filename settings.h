7548789113c667e58af1d8fedc00df9db13c3a1b
japodhidev <japodhidev@gmail.com>
2025-04-01 10:37:41 +0300

    feat: Use custom directory to avoid interference with `pywal`

--- settings.h
+++ settings.h
@@ -11,8 +11,8 @@ Util util;
 QString HOME = env.contains("HOME") ? env.value("HOME") : env.value("USERPROFILE");
 QString XDG_CACHE_DIR = env.contains("XDG_CACHE_HOME") ? env.value("XDG_CACHE_HOME") : util.joinPath(HOME, QString(".cache"));
 QString XDG_CONFIG_DIR = env.contains("XDG_CONFIG_HOME") ? env.value("XDG_CONFIG_HOME") : util.joinPath(HOME, QString(".config"));
-QString CACHE_DIR = env.contains("PYWAL_CACHE_DIR") ? env.value("PYWAL_CACHE_DIR") : util.joinPath(XDG_CACHE_DIR, QString("wal"));
-QString CONF_DIR = util.joinPath(XDG_CONFIG_DIR, QString("wal"));
+QString CACHE_DIR = env.contains("PYWAL_CACHE_DIR") ? env.value("PYWAL_CACHE_DIR") : util.joinPath(XDG_CACHE_DIR, QString("walpp"));
+QString CONF_DIR = util.joinPath(XDG_CONFIG_DIR, QString("walpp"));
 
 // TODO: Implement os.path.dirname(__file__)
 auto cPath = std::filesystem::current_path();
