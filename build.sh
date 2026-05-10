e9bb3c18e0865a7383c052ffd9dd58f50ac61fa9
japodhidev <japodhidev@gmail.com>
2026-05-10 22:23:19 +0300

    fix: removed build.sh

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_MAKE_PROGRAM=/opt/clion/bin/ninja/linux/x64/ninja -G Ninja -S /home/sleek/source/cpp/walpp -B /home/sleek/source/cpp/walpp/cmake-build-debug
-cmake --build /home/sleek/source/cpp/walpp/cmake-build-debug --target walpp -j 1
